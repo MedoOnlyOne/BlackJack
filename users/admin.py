7d80517befef09f84c4ac45fc7525160475f65af
Mohamed Medhat <mohamed.medhat2199@gmail.com>
2020-11-15 20:40:56 +0200

    Models Change

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models import User, Product
 # Register your models here.
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'remaininginstock')
     ordering = ['id']
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff')
+    ordering = ['-is_staff']
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Product, ProductAdmin)
--- users/models.py
+++ users/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
-class User(AbstractUser):
-    pass
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=7,decimal_places=2)
@@ -10,3 +10,10 @@ class Product(models.Model):
     remaininginstock = models.PositiveSmallIntegerField(default=1)
     def __str__(self):
         return '%s' %self.name
+
+class User(AbstractUser):
+    # every user will have 3 specified lists (parchases, cart, wishList)
+    parchases = models.ManyToManyField(Product, blank=True, related_name="parchases")
+    cart = models.ManyToManyField(Product, blank=True, related_name="cart")
+    wishList = models.ManyToManyField(Product, blank=True, related_name="wishList")
+
