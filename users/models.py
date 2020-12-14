cf297fa948f77d0432687618a4e9472673489fda
MedoOnlyOne <mohamed.medhat2199@gmail.com>
2020-12-14 14:28:42 +0200

    Change password

--- users/models.py
+++ users/models.py
@@ -10,7 +10,10 @@ class User(AbstractUser):
     phone_number_code=models.CharField(max_length=4,null=False,default="+20")
     country=models.CharField(max_length=30,null=False,default="Egypt")
     preferred_currency=models.CharField(max_length=3,null=False,default="EGP")
+    address = models.CharField(max_length=100,null=False,default="")
+    is_seller = models.BooleanField(default=False)
     parchases = models.ManyToManyField(Product, blank=True, related_name="Purchases")
     cart = models.ManyToManyField(Product, blank=True, related_name="Cart")
     wishList = models.ManyToManyField(Product, blank=True, related_name="WishList")
     shops = models.ManyToManyField(Shop,blank=True, related_name="OwnedShops")
+   
