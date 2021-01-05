50b2d7067b0efc7213288358e79f9b68ac824efa
Nabil <midorady9999@gmail.com>
2021-01-05 23:42:51 +0200

    pagination

--- search/urls.py
+++ search/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.search, name='search')
+    path('', views.search, name='search'),
+    path('a/',views.search_paginated,name='search_paginated')
 ]
--- search/views.py
+++ search/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from products.models import Product
 from shop.models import Shop
-from django.db.models import Q 
+from django.core.paginator import Paginator
 
 
 currency_symbols={
@@ -50,6 +50,40 @@ def search(request):
         results=sorted(results,key=lambda item:item.name)
         return render(request, 'search/searchResults.html',{
             'results': results,
+            'currency_ratio':currency_ratio,
+            'currency_symbol':currency_symbols[preferred_currency],
+            'is_products_search':False
+        })
+def search_paginated(request):
+    query = request.GET['search_name']
+    search_by = request.GET['search_for']
+    sort_by = request.GET.get('sort_by',None)
+    preferred_currency=get_preffered_currency(request)
+    currency_ratio=get_currency_ratio(request)
+    if search_by=='products':
+        results = Product.objects.filter(name__icontains=query)
+        if sort_by=='name':
+            results=sorted(results,key=lambda item:item.name)
+        else:
+            results=sorted(results,key=lambda item:item.price)
+        paginator=Paginator(results,5)
+        page_num=request.GET.get('page',1)
+        page_obj=paginator.get_page(page_num)
+        print('HOBA')    
+        return render(request, 'search/searchResults_paginated.html',{
+            'results': page_obj,
+            'currency_ratio':currency_ratio,
+            'currency_symbol':currency_symbols[preferred_currency],
+            'is_products_search':True
+        })
+    else:
+        results=Shop.objects.filter(name__icontains=query)
+        results=sorted(results,key=lambda item:item.name)
+        paginator=Paginator(results,5)
+        page_num=request.GET.get(['page'],1)
+        page_obj=paginator.get_page(page_num)    
+        return render(request, 'search/searchResults_paginated.html',{
+            'results': page_obj,
             'currency_ratio':currency_ratio,
             'currency_symbol':currency_symbols[preferred_currency],
             'is_products_search':False
