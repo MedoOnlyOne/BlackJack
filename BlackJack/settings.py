85aace3632da8e92ac34c468ec9c49bb48f2e853
Nabil <midorady9999@gmail.com>
2021-01-10 03:54:55 +0200

    env

--- BlackJack/settings.py
+++ BlackJack/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -154,3 +155,6 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'bj2080534@gmail.com' 
 EMAIL_HOST_PASSWORD = 'blackjack2020' 
+
+
+django_heroku.settings(locals())
