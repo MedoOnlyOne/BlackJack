a4f413c263761920696c59b8e611a2e41b9715fd
Nabil <midorady9999@gmail.com>
2021-01-10 14:28:09 +0200

    database settings

--- BlackJack/settings.py
+++ BlackJack/settings.py
@@ -95,9 +95,13 @@ CACHES = {
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
